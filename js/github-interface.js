2f5515fb4529e7a8da4ea3d68d2162580b755df8
LaTaevia Berry <lataevia.berry>
2016-05-31 09:10:34 -0700

    add bug, not sure why page keeps reloading

--- js/github-interface.js
+++ js/github-interface.js
@@ -3,11 +3,13 @@ var Github = require('./../js/github.js').GitHub;
 $(document).ready(function() {
 var currentGithubObject = new Github();
 //used click vs submit so page won't refresh
-  $('#ghUsernameSubmit').click(function() {
+  $('#ghUsernameClick').click(function() {
     var ghUsername = $('#ghUsernameInput').val();
-    $('#ghUsernameInput').val("");
     $('#github-user').text(ghUsername);
-    currentGithubObject.getGeneralUserInfo(ghUsername);
+    debugger;
+    // $('#ghUsernameInput').val("");
+    // debugger;
+    // currentGithubObject.getGeneralUserInfo(ghUsername);
   });
 
 });
