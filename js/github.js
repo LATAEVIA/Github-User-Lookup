6ea9c17aefa6b4f4e7897f1b1266504b1fbadd6e
LaTaevia Berry <lataevia.berry>
2016-05-31 10:50:59 -0700

    add function varibles to display data returned from api request, not yet working

--- js/github.js
+++ js/github.js
@@ -3,23 +3,24 @@ var apiKey = require('./../.env').apiKey;
 exports.GitHub = function() {
 };
 
-exports.GitHub.prototype.getGeneralUserInfo = function(ghUsername){
+exports.GitHub.prototype.getGeneralUserInfo = function(ghUsername, displayGeneralInfo, displayErrorMessage){
   $.get('https://api.github.com/users/' + ghUsername + '?access_token=' + apiKey).then(function(response) {
-    console.log(response);
+    displayGitHubInfo(ghusername, response.created_at, response.name, response.avatar_url, response.email, response.followers, response.public_repos, response.repos_url.full_name);
   }).fail(function(error){
+    displayErrorMessage(ghUsername);
     console.log(error.responseJSON.message);
   });
 };
 
 
-// exports.GitHub.prototype.getGeneralUserInfo = function(ghUsername, displayGeneralInfo, displayErrorMessage){
+// exports.GitHub.prototype.getGeneralUserInfo = function(ghUsername){
 //   $.get('https://api.github.com/users/' + ghUsername + '?access_token=' + apiKey).then(function(response) {
-//     displayGitHubInfo(ghusername, response.created_at, response.name, response.avatar_url, response.email, response.followers, response.public_repos, response.repos_url.full_name);
+//     console.log(response);
 //   }).fail(function(error){
 //     console.log(error.responseJSON.message);
 //   });
 // };
-
+//////////////////
 // var apiKey = require('./../.env').apiKey;
 //
 // exports.GitHub = function() {
