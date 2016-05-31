3620a979b3675752d900d6ac9bf6e640f426dc2b
LaTaevia Berry <lataevia.berry>
2016-05-31 08:58:10 -0700

    add changed to HTML and GH-interface to allow user to input GH username

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,4 +1,13 @@
 var Github = require('./../js/github.js').GitHub;
 
+$(document).ready(function() {
 var currentGithubObject = new Github();
-currentGithubObject.getGeneralUserInfo();
+//used click vs submit so page won't refresh
+  $('#ghUsernameSubmit').click(function() {
+    var ghUsername = $('#ghUsernameInput').val();
+    $('#ghUsernameInput').val("");
+    $('#github-user').text(ghUsername);
+    currentGithubObject.getGeneralUserInfo();
+  });
+
+});
