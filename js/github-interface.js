26a9615aeb4811df5e3259f3639b94b2859d0ef0
LaTaevia Berry <lataevia.berry>
2016-05-31 09:56:13 -0700

    resolve refresh bug. didn't give HTML #ghUsernameLookip button a type value so it defaulted to submit.

--- js/github-interface.js
+++ js/github-interface.js
@@ -4,12 +4,18 @@ $(document).ready(function() {
 var currentGithubObject = new Github();
 //used click vs submit so page won't refresh
   $('#ghUsernameClick').click(function() {
+    
     var ghUsername = $('#ghUsernameInput').val();
     $('#github-user').text(ghUsername);
     debugger;
-    // $('#ghUsernameInput').val("");
-    // debugger;
-    // currentGithubObject.getGeneralUserInfo(ghUsername);
+    $('#ghUsernameInput').val("");
+    debugger;
+    currentGithubObject.getGeneralUserInfo(ghUsername);
   });
 
 });
+//
+// var Github = require('./../js/github.js').GitHub;
+//
+// var currentGithubObject = new Github();
+// currentGithubObject.getGeneralUserInfo();
--- js/github.js
+++ js/github.js
@@ -10,3 +10,25 @@ exports.GitHub.prototype.getGeneralUserInfo = function(ghUsername){
     console.log(error.responseJSON.message);
   });
 };
+
+
+// exports.GitHub.prototype.getGeneralUserInfo = function(ghUsername, displayGeneralInfo, displayErrorMessage){
+//   $.get('https://api.github.com/users/' + ghUsername + '?access_token=' + apiKey).then(function(response) {
+//     displayGitHubInfo(ghusername, response.created_at, response.name, response.avatar_url, response.email, response.followers, response.public_repos, response.repos_url.full_name);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
+
+// var apiKey = require('./../.env').apiKey;
+//
+// exports.GitHub = function() {
+// };
+//
+// exports.GitHub.prototype.getGeneralUserInfo = function(){
+//   $.get('https://api.github.com/users/lataevia?access_token=' + apiKey).then(function(response){
+//     console.log(response);
+//   }).fail(function(error){
+//     console.log(error.responseJSON.message);
+//   });
+// };
