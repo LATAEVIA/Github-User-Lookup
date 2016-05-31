14f4af4a1c707d9696b0e0d06bd3efb873bced6e
LaTaevia Berry <lataevia.berry>
2016-05-31 08:27:05 -0700

    add basic HTML layout, no styles and working api example

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,11 +1,3 @@
-var Github = require('./../js/github.js').Github;
+var Github = require('./../js/github.js').getRepos;
 
-var displayRepos = function(nameData, descriptionData) {
-  $('#repoNameAndDescription').text(nameData + "<br>" + descriptionData);
-}
-
-$(document).ready(function() {
-  var currentGithubObject = new Github();
-    currentGithubObject.getRepos();
-});
-// 469b240047d186bc5eeedf7bbaa6e113e2ca2fc9
+var currentGithubObject = new Github();
