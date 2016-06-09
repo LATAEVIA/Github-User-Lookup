49cd54a71075521ea8bcf21fc153ed2a885ba09c
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-08 19:12:25 -0700

    add to appearance of github repositories. Need to fix bug

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,5 +1,20 @@
 var Github = require('./../js/github.js').GitHub;
 
+var displayRepoInfo = function(repoName, repoDescription, dateCreated) {
+  $('#repoNameAndDescription').append(
+    "<div class="panel panel-default">" +
+      "<div class="panel-heading" >" +
+        "<h3 class="panel-title">" + repoName + "<br>" + moment(dateCreated).format('dddd, MMMM Do YYYY, h:mm:ss a') +
+        "</h3>" +
+      "</div>" +
+      "<div class="panel-body">" +
+        repoDescription +
+      "</div>" +
+    "</div>");
+};
+
+
+
 var displayGeneralInfo = function(username, acct_creation, full_name, avatar_url, email, followers, public_repos, repos_url) {
   $('#userNameAndDescription').html(username + "<br>" + acct_creation + "<br>" + full_name + "<br>" + avatar_url + "<br>" + email + "<br>" + followers + "<br>" + public_repos + "<br>" + repos_url);
 };
@@ -9,18 +24,17 @@ var displayErrorMessage = function(ghUsername) {
   $('#userNameAndDescription').text("Username " + ghUsername + " could not be found. Please check spelling and try looking up again");
 };
 
+
+
 $(document).ready(function() {
 var currentGithubObject = new Github();
+var currentGithubObject2 = new Github();
 //used click vs submit so page won't refresh
   $('#ghUsernameClick').click(function() {
     var ghUsername = $('#ghUsernameInput').val();
-    $('#github-user').text(ghUsername);
+    $('#github-user').text(ghUsername).show();
     $('#ghUsernameInput').val("");    currentGithubObject.getGeneralUserInfo(ghUsername, displayGeneralInfo, displayErrorMessage);
+    currentGithubObject2.getRepoInfo(ghUsername, displayRepoInfo, displayErrorMessage);
   });
 
 });
-//
-// var Github = require('./../js/github.js').GitHub;
-// exports.apiKey = "469b240047d186bc5eeedf7bbaa6e113e2ca2fc9";
-// var currentGithubObject = new Github();
-// currentGithubObject.getGeneralUserInfo();
--- js/github.js
+++ js/github.js
@@ -3,35 +3,22 @@ var apiKey = require('./../.env').apiKey;
 exports.GitHub = function() {
 };
 
+exports.GitHub.prototype.getRepoInfo = function(ghUsername, displayRepoInfo, displayErrorMessage){
+  $.get('https://api.github.com/users/' + ghUsername + '/repos').then(function(response) {
+    for (var i = 0; i < response.length; i += 1) {
+      displayRepoInfo(response[i].name, response[i].description, response[i].created_at);
+    }
+  }).fail(function(error){
+    displayErrorMessage(ghUsername);
+    console.log(error.responseJSON.message);
+  });//customize
+};
+
 exports.GitHub.prototype.getGeneralUserInfo = function(ghUsername, displayGeneralInfo, displayErrorMessage){
   $.get('https://api.github.com/users/' + ghUsername + '?access_token=' + apiKey).then(function(response) {
-    displayGeneralInfo(ghUsername, response.created_at, response.name, response.avatar_url, response.email, response.followers, response.public_repos, response.repos_url.full_name);
-    //repo_url?
-
+    displayGeneralInfo(ghUsername, response.created_at, response.name, response.avatar_url, response.email, response.followers, response.public_repos);
   }).fail(function(error){
     displayErrorMessage(ghUsername);
     console.log(error.responseJSON.message);
   });
 };
-
-
-// exports.GitHub.prototype.getGeneralUserInfo = function(ghUsername){
-//   $.get('https://api.github.com/users/' + ghUsername + '?access_token=' + apiKey).then(function(response) {
-//     console.log(response);
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// };
-//////////////////
-// var apiKey = require('./../.env').apiKey;
-//
-// exports.GitHub = function() {
-// };
-//
-// exports.GitHub.prototype.getGeneralUserInfo = function(){
-//   $.get('https://api.github.com/users/lataevia?access_token=' + apiKey).then(function(response){
-//     console.log(response);
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// };
